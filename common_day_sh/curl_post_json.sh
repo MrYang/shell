a874b962892845a8c54438170bb03577425a1144
yxb_1990 <657048548@qq.com>
2016-04-01 12:20:52 +0800

    删除目录common_day_sh

--- common_day_sh/curl_post_json.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl -H "Content-Type: application/json; charset=UTF-8" -X POST -d '{"data":"data"}' http://ip:port
-curl -F "photo=@/home/photo.jpg" -X POST -F 'userId=1' http://ip:port
